7d938cff6d903b87491e917c956e7f141959e598
cheikh tourad <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-02-21 17:46:51 +0100

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(UserRepository $userRepository): Response
+    {
+
+        $user=$userRepository->findOneBy(['email'=>'gheith@test.com']);
+
+
+        return $this->render('home/index.html.twig', [
+            'nom' => $user->getNom(),
+        ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+    {
+        #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', options: ['unsigned' => true])]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    private string $nom;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    private string $prenom;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Assert\Email]
+    private string $email;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $numero = null;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    #[Assert\Choice(choices: ['patient', 'medecin', 'pharmacie', 'infermier', 'admin'], message: 'Choisissez un rôle valide.')]
+    private string $role;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $password;
+
+    // Getters and Setters
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?string $numero): self
+    {
+        $this->numero = $numero;
+        return $this;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
