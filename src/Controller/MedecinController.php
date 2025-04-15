2abf62c10be2e4095425602f1008896e854c32ef
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-04-15 13:50:22 +0100

    medecin patient admin

--- src/Controller/MedecinController.php
+++ src/Controller/MedecinController.php
@@ -10,11 +10,7 @@ final class MedecinController extends AbstractController
 {
     #[Route('/medecin/', name: 'app_home_medecin')]
     public function indexMedecin(): Response{
-        $user = $this->getUser();
-        if(!$user){
-            return $this->redirectToRoute('app_login');
-        }
-        $nom=$this->getUser()->getNom();
-        return $this->render('medecin/index.html.twig', ['nom'=>$nom]);
+
+        return $this->render('medecin/index.html.twig');
     }
 }
