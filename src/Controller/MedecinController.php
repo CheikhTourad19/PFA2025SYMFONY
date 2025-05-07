2d74fcbe171c66d7981c20f423bf79df629592c5
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-05-07 23:07:40 +0100

    messages socket real time with nice  UI

--- src/Controller/MedecinController.php
+++ src/Controller/MedecinController.php
@@ -37,7 +37,7 @@ final class MedecinController extends AbstractController
             return $this->redirectToRoute('app_medecin_profil');
 
         }
-        return $this->redirectToRoute('app_task_index');
+        return $this->render('medecin/home.html.twig');
     }
     #[Route('/ordonnance', name: 'app_medecin_ordonnace', methods: ['GET'])]
     public function ordonnanceMedecin(MedicamentRepository $medicamentRepository,UserRepository $userRepository): Response
