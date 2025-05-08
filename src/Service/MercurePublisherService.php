082ac97b2ae7facf1a61e877f55cfea2cfec0d63
CheikhTourad19 <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-05-08 13:43:05 +0100

    mercure

--- src/Service/MercurePublisherService.php
+++ src/Service/MercurePublisherService.php
@@ -4,6 +4,7 @@
 namespace App\Service;
 
 use App\Entity\Message;
+use App\Entity\User;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 
@@ -27,6 +28,20 @@ class MercurePublisherService
         ]);
 
         // Create and publish the update
+        $update = new Update($topic, $data);
+        $this->hub->publish($update);
+    }
+    // In MercurePublisherService.php
+    public function publishUserStatus(User $user, bool $isOnline): void
+    {
+        $topic = 'user/status';
+
+        $data = json_encode([
+            'userId' => $user->getId(),
+            'isOnline' => $isOnline,
+            'timestamp' => (new \DateTime())->format('Y-m-d H:i:s')
+        ]);
+
         $update = new Update($topic, $data);
         $this->hub->publish($update);
     }
